7ee1057b4ff1b425b03df19d3289a5590a578869
Honghao Wang <hongwang@ebay.com>
2018-09-07 22:59:13 -0400

    add ff support

--- Tools/youguan.py
+++ Tools/youguan.py
@@ -14,24 +14,57 @@ import logging; LOG = logging.getLogger(__name__)
 LOGFMT = '[%(asctime)s %(filename)s:%(lineno)d] %(message)s'
 LOGDATEFMT = '%Y%m%d-%H:%M:%S'
 
-def getChromeDriver(arguments=[], proxy=""):
-    #options = webdriver.ChromeOptions()
-    #options.add_argument('--ignore-certificate-errors')
-    #options.add_argument("--test-type")
-    #options.binary_location = "/Users/hongwang/Bin/chromium"
-    #options.add_argument("user-data-dir=" + profilePath)
-    #options.add_argument("headless")
-    #options.add_argument('window-size=1920x1080')
-    #options.add_argument('--proxy-server=%s' % PROXY)
+class FireFoxDriverBuilder(object):
+    def __init__(self, proxy=''):
+        self.proxy = proxy
+    
+    def setProxy(self, proxy):
+        self.proxy = proxy
+        return self
+    
+    def getDriver(self):
+        profile = webdriver.FirefoxProfile()
+        if self.proxy:
+            host, port = self.proxy.split(":")
+            profile.set_preference("network.proxy.type", 1)
+            profile.set_preference("network.proxy.http", host)
+            profile.set_preference("network.proxy.http_port", port)
+            profile.set_preference("network.proxy.ssl", host)
+            profile.set_preference("network.proxy.ssl_port", port)
 
-    options = webdriver.ChromeOptions()
-    for arg in arguments:
-        options.add_argument(arg)
+        return webdriver.Firefox(firefox_profile=profile)
+
+class ChromeDriverBuilder(object):
+    def __init__(self, proxy=''):
+        self.arguments = ['--ignore-certificate-errors']
+        self.proxy = proxy
     
-    if proxy:
-        options.add_argument('--proxy-server=%s' % proxy)
+    def setProxy(self, proxy):
+        self.proxy = proxy
+        return self
     
-    return webdriver.Chrome(chrome_options=options)
+    def setArguments(self, arguments):
+        self.arguments = arguments
+        return self
+
+    def getDriver(self):
+        #options = webdriver.ChromeOptions()
+        #options.add_argument('--ignore-certificate-errors')
+        #options.add_argument("--test-type")
+        #options.binary_location = "/Users/hongwang/Bin/chromium"
+        #options.add_argument("user-data-dir=" + profilePath)
+        #options.add_argument("headless")
+        #options.add_argument('window-size=1920x1080')
+        #options.add_argument('--proxy-server=%s' % PROXY)
+
+        options = webdriver.ChromeOptions()
+        for arg in self.arguments:
+            options.add_argument(arg)
+        
+        if self.proxy:
+            options.add_argument('--proxy-server=%s' % self.proxy)
+        
+        return webdriver.Chrome(chrome_options=options)
 
 def waitPlay(driver, url, minutes, locator="", waitlocator=60, dryrun=False):
     LOG.info("Playing url=%s minutes=%s" % (url, minutes))
@@ -45,7 +78,7 @@ def waitPlay(driver, url, minutes, locator="", waitlocator=60, dryrun=False):
         if locator:
             LOG.info("Checking Page loaded")
             WebDriverWait(driver, waitlocator).until(
-                EC.presence_of_element_located((By.ID, "logo-icon-container")))
+                EC.presence_of_element_located((By.ID, locator)))
 
         for x in range(minutes):
             time.sleep(60)
@@ -60,16 +93,22 @@ def parseArguments():
 
     parser.add_argument('-f', '--linkfile', help='File include link and minutes in one line')
     parser.add_argument('-p', '--proxyfile', help='File include proxy and port each line')
-    parser.add_argument('-n', '--numberoftimes', help='number of times to run', type=int, default=1)
+    parser.add_argument('-n', '--numberoftimes', help='Number of times to run', type=int, default=1)
+    parser.add_argument('-b', '--browsers', help='Browsers to be used', default='chrome')
 
     return parser.parse_args()
 
 logging.basicConfig(format=LOGFMT,datefmt=LOGDATEFMT)
 logging.getLogger().setLevel(logging.INFO)
 
+fireFoxDriverBuilder = FireFoxDriverBuilder()
+chromeDriverBuilder = ChromeDriverBuilder()
+
 args = parseArguments()
 #print(args.linkfile)
 #print(args.proxyfile)
+
+lstBrowsers = args.browsers.split(',')
 
 lstUrl = []
 if args.linkfile:
@@ -90,12 +129,18 @@ if args.proxyfile:
             if not one or one[0] == '#': continue
             lstProxy.extend([one]*args.numberoftimes)
 else:
-    lstProxy.append([""]*args.numberoftimes)
+    lstProxy.extend([""]*args.numberoftimes)
 
-lstArgs = ['--ignore-certificate-errors']
+#lstArgs = ['--ignore-certificate-errors']
 random.shuffle(lstProxy)
 for proxy in lstProxy:
     LOG.info("Using Proxy [%s]" % proxy)
     for one in lstUrl:
-        driver = getChromeDriver(lstArgs, proxy)
+        browser = random.choice(lstBrowsers).lower()
+        builder = chromeDriverBuilder
+        if browser == 'firefox':
+            builder = fireFoxDriverBuilder
+        
+        LOG.info(proxy)
+        driver = builder.setProxy(proxy).getDriver()
         waitPlay(driver, one[0], one[1], "logo-icon-container")
