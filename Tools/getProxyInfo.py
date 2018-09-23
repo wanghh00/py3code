7b823373e14d33e24690db26e30f5dd0abb896c8
Honghao Wang <hongwang@ebay.com>
2018-09-22 22:27:14 -0400

    hehe

--- Tools/getProxyInfo.py
+++ Tools/getProxyInfo.py
@@ -144,14 +144,22 @@ fireFoxDriverBuilder = FireFoxDriverBuilder()
 chromeDriverBuilder = ChromeDriverBuilder()
 
 SPYSURLS = ["http://spys.one/free-proxy-list/US/", "http://spys.one/free-proxy-list/US/1/"]
-US_PROXY_ORG = "https://www.us-proxy.org/"
+US_PROXY_ORG = ["https://www.us-proxy.org/"]
 
-
-#with open('/tmp/hehe.html') as fp:
-#    soup = BeautifulSoup(fp.read(), 'html.parser')
-#    print(soup.prettify())
-
-#sys.exit(0)
+def parseHtmlFromUSProxyOrg(html):
+    ret = []
+    try:
+        soup = BeautifulSoup(html, 'html.parser')
+        table = soup.find('table', id='proxylisttable')
+        for row in table.find_all('tr'):
+            tds = row.find_all('td')
+            if len(tds) < 8 or not tds[2].contents[0] == 'US' or not tds[4].contents[0] == 'elite proxy' \
+                    or not tds[6].contents[0] == 'yes':
+                continue
+            ret.append("%s:%s" % (tds[0].contents[0], tds[1].contents[0]))
+    except Exception as ex:
+        LOG.exception(ex)
+    return ret
 
 def parseSPYS(driver):
     ret = []
@@ -165,21 +173,26 @@ def parseSPYS(driver):
     return ret
 
 
-driver = chromeDriverBuilder.getDriver()
-
 outFile, driver = None, None
 try:
     outFile = open(sys.argv[1], 'w')
-    driver = chromeDriverBuilder.getDriver()
 
+    # handle US_PROXY_ORG
+    for one in US_PROXY_ORG:
+        outFile.write("\n#%s\n" % one)
+        for proxy in parseHtmlFromUSProxyOrg(requests.get(one).text):
+            outFile.write("%s\n" % proxy)
+    
+    # handle SPYSURLS
+    driver = chromeDriverBuilder.getDriver()
     for one in SPYSURLS:
-        outFile.write("#%s\n" % one)
+        outFile.write("\n#%s\n" % one)
         driver.get(SPYSURLS[0])
         for proxy in parseSPYS(driver):
             outFile.write("%s\n" % proxy)
+
 except Exception as ex:
     LOG.exception(ex)
 finally:
     driver and driver.close()
     outFile and outFile.close()
-
